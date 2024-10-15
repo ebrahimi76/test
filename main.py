525f8ccf296a9273867904feddd6d721d8ce632f
ebrahimi76 <ebrahimimohammadali76@gmail.com>
2024-10-15 17:39:52 +0330

    Five commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def print_hi(name):
-    print(f'Hi, your name is: {name}')
-    print('have a nice day!')
+    print(f"Hi, your name is: {name}")
+    print("have a nice day!")
+    print(":)")
 
 
-if __name__ == '__main__':
-    print_hi(input('What is your name? ').capitalize())
+if __name__ == "__main__":
+    print_hi(input("What is your name? ").capitalize())
